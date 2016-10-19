b12ec5eb32f6e5bf6a54c474a981a9c345934892
Javier Torres <javier.ta2593@gmail.com>
2016-10-18 20:11:33 -0700

    Update app.js

--- shared/app.js
+++ shared/app.js
@@ -3,83 +3,81 @@
 var main = function () 
 { 
 	'use strict';
-    var url = '';
+    	var url = '';
 
 	var getRoute = function () 
-    {
+    	{
 		var input = $('.route input').val();
+		if ( input === '/average' || input === '/max' || input === '/isEven' || input === '/isEveryNumEven' ) 
+        	{
+        		url = 'http://localhost:3000';
+            		url = url + input;
 
-    	if ( input === '/average' || input === '/max' || input === '/isEven' || input === '/isEveryNumEven' ) 
-        {
-        	
-        	url = 'http://localhost:3000';
-            url = url + input;
-
-            $.ajax(
-            {
-    			url: url, 
-    			type: 'post',
-            	dataType: 'json',
-            	data: JSON.stringify({"array": "[1,2,3,4,5]" }),
-            	contentType: 'application/json',
-            	success: function(data)
-                {
-                	$('#output-result').val(data.answer);
-       			}
+            		$.ajax(
+            		{
+    				url: url, 
+    				type: 'post',
+            			dataType: 'json',
+            			data: JSON.stringify({"array": "[1,2,3,4,5]" }),
+            			contentType: 'application/json',
+            			success: function(data)
+                		{
+                			$('#output-result').val(data.answer);
+       				}
 			});
-        }
+        	}
 
-        else if( input === '/arrayContains' )
-        {
-            url = 'http://localhost:3000';
-            url = url + input;
+        	else if( input === '/arrayContains' )
+        	{
+            		url = 'http://localhost:3000';
+            		url = url + input;
         	
-            $.ajax(
-            {
-    			url: url, 
-    			type: 'post',
-            	dataType: 'json',
-            	data: JSON.stringify({"array":"['hello','world']", "arg2":"hello"}),
-            	contentType: 'application/json',
-            	success: function(data)
-                {
-                    $('#output-result').val(data.answer);
-       			}
+            		$.ajax(
+            		{
+    				url: url, 
+    				type: 'post',
+            			dataType: 'json',
+            			data: JSON.stringify({"array":"['hello','world']", "arg2":"hello"}),
+            			contentType: 'application/json',
+            			success: function(data)
+                		{
+                    			$('#output-result').val(data.answer);
+       				}
 			});
 
-        }
+        	}
 
-        else if( input === '/arrayContainsTwo' )
-        {
-            url = 'http://localhost:3000';
-            url = url + input;
+        	else if( input === '/arrayContainsTwo' )
+        	{
+            		url = 'http://localhost:3000';
+            		url = url + input;
         	
-            $.ajax(
-            {
-    			url: url, 
-    			type: 'post',
-            	dataType: 'json',
-            	data: JSON.stringify({"array":"['a','b','a','c']", "arg2":"c"}),
-            	contentType: 'application/json',
-            	success: function(data)
-                {
-                	$('#output-result').val(data.answer);
-       			}
+            		$.ajax(
+            		{
+    				url: url, 
+    				type: 'post',
+            			dataType: 'json',
+            			data: JSON.stringify({"array":"['a','b','a','c']", "arg2":"c"}),
+            			contentType: 'application/json',
+            			success: function(data)
+                		{
+                			$('#output-result').val(data.answer);
+       				}
 			});
-        }
+        	}
 
-        //resets the input box to default string
-        $('#input-function').val('');
+        	//resets the input box to default string
+        	$('#input-function').val('');
 
 	};
 
 
 	$('.route button').on('click', function (event) 
-    {
-    	getRoute();
+    	{
+    		getRoute();
 	});
     
-    url = '';
+    	url = '';
 };
 
 $(document).ready(main);
